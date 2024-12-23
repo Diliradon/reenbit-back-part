21b8165148555fbbc1bf5ba835a4ca622f162aeb
Vladyslav_Zakharov <vladyslavzakharovdk@gmail.com>
2024-12-23 21:21:28 +0100

    add cors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import 'dotenv/config'
 import express from 'express'
 import authRouter from './routes/auth.route.js'
 import { connectDB } from './utils/db.js'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 3000
+const CLIENT_HOST = process.env.CLIENT_HOST
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({
+  origin: CLIENT_HOST,
+  credentials: true
+}))
 
 app.use('/auth', authRouter)
 
@@ -17,10 +24,20 @@ app.get('/', (req, res) => {
 
 // Connect to database before starting the server
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-  })
+  const server = app.listen(PORT)
+    .on('error', (error) => {
+      if (error.code === 'EADDRINUSE') {
+        console.error(`Port ${PORT} is already in use. Please try another port.`)
+        process.exit(1)
+      } else {
+        console.error('Failed to start server:', error)
+        process.exit(1)
+      }
+    })
+    .on('listening', () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
 }).catch(error => {
-  console.error('Failed to start server:', error)
+  console.error('Failed to connect to database:', error)
   process.exit(1)
 })
